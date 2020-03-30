b69e4cdc91d6f2d346c9777456a2e955ccbf1950
Ahmadreza Ghanbari <r.gh687@gmail.com>
2020-03-30 14:33:21 +0430

    HomeWork_1 Update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,24 +12,3 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 }
-class user extends Controller
-{
-    public $id;
-    public $Name;
-    public $Email;
-    public function __constructor($id,$Name,$Email){
-        $this->id = $id;
-        $this->Name = $Name;
-        $this->Email = $Email;
-    }
-    public function update($id,$Name,$Email){
-        $this->id = $id;
-        $this->Name = $Name;
-        $this->Email = $Email;
-    }
-    public function delete(){
-        $this->id = null;
-        $this->Name = null;
-        $this->Email = null;
-    }
-}
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public $id;
+    public $Name;
+    public $Email;
+    public function __constructor($id,$Name,$Email){
+        $this->id = $id;
+        $this->Name = $Name;
+        $this->Email = $Email;
+    }
+    public function update($id,$Name,$Email){
+        $this->id = $id;
+        $this->Name = $Name;
+        $this->Email = $Email;
+    }
+    public function delete(){
+        $this->id = null;
+        $this->Name = null;
+        $this->Email = null;
+    }
 }
+
