01c4f69963880e51915b57f011b0d96aceac6c21
Ahmadreza Ghanbari <r.gh687@gmail.com>
2020-03-31 14:28:50 +0430

    Update SQL

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class StudentController extends Controller
 {
     public $id;
@@ -13,16 +13,33 @@ class StudentController extends Controller
         $this->id = $id;
         $this->Name = $Name;
         $this->Email = $Email;
+        DB::table('students')->insert([
+            'Name' => $this->Name,
+            'Email' => $this->Email
+            ]);
     }
     public function update($id,$Name,$Email){
         $this->id = $id;
         $this->Name = $Name;
         $this->Email = $Email;
+        DB::table('students')->where('id', $this->id)
+        ->update([
+            'Name' => $this->Name,
+            'Email' => $this->Email
+            ]);
     }
     public function delete(){
         $this->id = null;
         $this->Name = null;
         $this->Email = null;
+        DB::table('students')->where('id', '=', $this->id)->delete();
     }
+    public function get_student($id){
+        $user = DB::table('students')->whereid($id);
+        $this->id = $user->id;
+        $this->Name = $user->Name;
+        $this->Email = $user->Email;
+    }
+
 }
 
